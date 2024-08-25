53a2f85641487b307957b668b0cb86f2ff5c7691
madeinflower <tatsenko.tetiana@gmail.com>
2024-08-25 14:52:33 +0200

    HW8

---
+++ HW_8.sql
@@ -0,0 +1,100 @@
+CREATE DATABASE shop;
+
+USE shop;
+
+CREATE TABLE SELLERS(
+       SELL_ID    INTEGER, 
+       SNAME   VARCHAR(20), 
+       CITY    VARCHAR(20), 
+       COMM    NUMERIC(2, 2),
+             BOSS_ID  INTEGER
+);
+                                            
+CREATE TABLE CUSTOMERS(
+       CUST_ID    INTEGER, 
+       CNAME   VARCHAR(20), 
+       CITY    VARCHAR(20), 
+       RATING  INTEGER
+);
+
+CREATE TABLE ORDERS(
+       ORDER_ID  INTEGER, 
+       AMT     NUMERIC(7,2), 
+       ODATE   DATE, 
+       CUST_ID    INTEGER,
+       SELL_ID    INTEGER 
+);
+
+INSERT INTO SELLERS VALUES(201,'Олег','Москва',0.12,202);
+INSERT INTO SELLERS VALUES(202,'Лев','Сочи',0.13,204);
+INSERT INTO SELLERS VALUES(203,'Арсений','Владимир',0.10,204);
+INSERT INTO SELLERS VALUES(204,'Екатерина','Москва',0.11,205);
+INSERT INTO SELLERS VALUES(205,'Леонид ','Казань',0.15,NULL);
+
+
+INSERT INTO CUSTOMERS VALUES(301,'Андрей','Москва',100);
+INSERT INTO CUSTOMERS VALUES(302,'Михаил','Тула',200);
+INSERT INTO CUSTOMERS VALUES(303,'Иван','Сочи',200);
+INSERT INTO CUSTOMERS VALUES(304,'Дмитрий','Ярославль',300);
+INSERT INTO CUSTOMERS VALUES(305,'Руслан','Москва',100);
+INSERT INTO CUSTOMERS VALUES(306,'Артём','Тула',100);
+INSERT INTO CUSTOMERS VALUES(307,'Юлия','Сочи',300);
+
+
+INSERT INTO ORDERS VALUES(101,18.69,'2022-03-10',308,207);
+INSERT INTO ORDERS VALUES(102,5900.1,'2022-03-10',307,204);
+INSERT INTO ORDERS VALUES(103,767.19,'2022-03-10',301,201);
+INSERT INTO ORDERS VALUES(104,5160.45,'2022-03-10',303,202);
+INSERT INTO ORDERS VALUES(105,1098.16,'2022-03-10',308,207);
+INSERT INTO ORDERS VALUES(106,75.75,'2022-04-10',304,202); 
+INSERT INTO ORDERS VALUES(107,4723,'2022-05-10',306,201);
+INSERT INTO ORDERS VALUES(108,1713.23,'2022-04-10',302,203);
+INSERT INTO ORDERS VALUES(109,1309.95,'2022-06-10',304,203);
+INSERT INTO ORDERS VALUES(110,9891.88,'2022-06-10',306,201);
+
+
+-- Tasks
+
+ -- 1. Определить какие из покупок были совершены в июне
+ 
+SELECT * 
+FROM ORDERS 
+WHERE MONTH(ODATE) = 6 
+AND YEAR(ODATE) = 2022;
+
+-- 2. Определить какие из покупок были совершены в интервале от 10 апреля 2022 до 10 мая 2022 года
+
+SELECT * 
+FROM ORDERS 
+WHERE ODATE BETWEEN '2022-04-10' AND '2022-05-10';
+
+-- 3. Определить сколько покупок было совершено в марте
+
+SELECT COUNT(*) AS NumberOfPurchases 
+FROM ORDERS 
+WHERE MONTH(ODATE) = 3 
+AND YEAR(ODATE) = 2022;
+
+-- 4. Определить среднюю стоимость покупок в марте
+
+SELECT AVG(AMT) AS AveragePurchaseAmount 
+FROM ORDERS 
+WHERE MONTH(ODATE) = 3 
+AND YEAR(ODATE) = 2022;
+
+-- 5. Выведите минимальный рейтинг покупателя среди сделавших заказ в апреле 2022 года
+
+SELECT MIN(C.RATING) AS MinimumRating 
+FROM ORDERS O
+JOIN CUSTOMERS C ON O.CUST_ID = C.CUST_ID
+WHERE MONTH(O.ODATE) = 4 
+AND YEAR(O.ODATE) = 2022;
+
+-- 6. Выведите продавцов, которые оформили заказ на наибольшую сумму в период с 1 марта по 1 май 2022 года
+
+SELECT SELL_ID, SUM(AMT) AS TotalAmount 
+FROM ORDERS 
+WHERE ODATE BETWEEN '2022-03-01' AND '2022-05-01'
+GROUP BY SELL_ID
+ORDER BY TotalAmount DESC
+LIMIT 1;
